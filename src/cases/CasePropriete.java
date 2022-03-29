e387c1481a2ac4777acb674b8e2225f6a881ef8f
toussaintcarlotti <toussaint.carlotti@gmail.com>
2022-03-29 15:41:40 +0200

    mise en place case propriétaire

---
+++ src/cases/CasePropriete.java
@@ -0,0 +1,74 @@
+package cases;
+
+import joueur.Joueur;
+
+import java.util.Scanner;
+
+public class CasePropriete extends Case {
+	
+	private Joueur proprietaire;
+	private int loyer=200;
+	private int prixAchat=1000;
+	private String nom;
+	
+	public CasePropriete() {
+		super();
+	}
+
+	@Override
+	public void action(Joueur j) {
+		// TODO Auto-generated method stub
+		if(proprietaire!=null) {
+			if(j!=proprietaire) {
+				payerLoyer(j);
+			}
+		}
+		else {
+			if(j.getCompte()>=prixAchat) {
+				proposerAchat(j);
+			}
+		}
+	}
+
+	private void proposerAchat(Joueur j) {
+		// TODO Auto-generated method stub
+		System.out.println(j+"peut acheter "+this+"[Y/N]");
+		boolean c=input(j);
+		if(c) {
+			setProprietaire(j);
+		}
+		j.takeFromCompte(prixAchat);
+	}
+	
+	private void setProprietaire(Joueur j) {
+		// TODO Auto-generated method stub
+		proprietaire=j;
+	}
+
+	public boolean input(Joueur j) {
+		boolean res=true;
+		String input="";
+		Scanner scanner=new Scanner(System.in);
+		input=scanner.nextLine();
+		if(input=="Y") {
+			res= true;
+		}
+		else if(input=="N") {
+			res= false;
+		}
+		return res;
+		
+		
+	}
+
+	private void payerLoyer(Joueur j) {
+		// TODO Auto-generated method stub
+		System.out.println(j+" paye "+loyer+" de loyer");
+		j.takeFromCompte(loyer);
+	}
+	
+	public String toString() {
+		return super.toString()+" prix: "+prixAchat+", loyer: "+loyer;
+	}
+
+}
