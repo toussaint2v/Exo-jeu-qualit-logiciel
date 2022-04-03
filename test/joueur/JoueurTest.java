69749f887d2a75ce853590d60316fbfacf1448c7
n-ottavi-n <nottavi14@gmail.com>
2022-04-03 15:30:28 +0200

    testJoueur junit

--- test/joueur/JoueurTest.java
+++ test/joueur/JoueurTest.java
@@ -1,34 +1,64 @@
-package joueur;
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import cases.Case;
+import cases.CasePropriete;
+import joueur.Joueur;
 import structure.Pion;
+import structure.Plateau;
 
-class JoueurTest {
-    static Joueur joueur;
-    @BeforeAll
-    static void init(){
-        Pion pion = new Pion("blue");
-        joueur = new Joueur("pierre", pion);
-    }
-    @Test
-    void deplacer() {
+class joueurTest {
+	
+	private final Pion p=new Pion("rouge");
+	private final Joueur j=new Joueur("jean",p);
+	private final Case c1=new CasePropriete();
+	private final Case c2=new CasePropriete();
+	private final Plateau plateau=new Plateau();
+	
+	
+	
+	
 
-    }
+	@DisplayName("test getNom=jean")
+	@Test
+	void testGetNom() {
+		j.getNom();
+		assertEquals("jean",j.getNom());
+	}
+	
+	@DisplayName("test setCase=1")
+	@Test
+	void testSetCase() {
+		j.setCase(c1);
+		assertEquals(c1.getCaseId(),j.getCase());
+	}
+	
+	@DisplayName("test add to compte +500")
+	@Test
+	void testAddToCompte() {
+		j.addToCompte(500);
+		assertEquals(2000,j.getCompte());
+	}
+	
+	@DisplayName("test année prison")
+	@Test
+	void testPrison() {
+		j.allerPrison();
+		assertEquals(1,j.getAnnePrison());
+		j.sortirPrison();
+		assertEquals(0,j.getAnnePrison());		
+	}
+	
+	
+	
+	
+	
+	
+	
 
-    @Test
-    void allerPrison() {
-    }
-
-    @Test
-    void sortirPrison() {
-    }
-
-    @Test
-    void addToCompte() {
-    }
-
-    @Test
-    void takeFromCompte() {
-    }
 }
